b401abb3517a6463d43f0f0fb2c0a656e2443b83
Elizabeth Gagne <EGagne-c@scrippsnetworks.com>
2017-06-20 21:10:43 -0400

    Added common.groovy

---
+++ common.groovy
@@ -0,0 +1,37 @@
+import jenkins.model.*
+
+def stepsForParallel = [:]
+
+@NonCPS
+def jobs(jobRegexp) {
+  Jenkins.instance.getAllItems()
+         .grep { it.name ==~ ~"${jobRegexp}"  }
+         .collect { [ name : it.name.toString(),
+                      fullName : it.fullName.toString() ] }
+}
+
+
+def transformIntoStep(jobFullName) {
+    // We need to wrap what we return in a Groovy closure, or else it's invoked
+    // when this method is called, not when we pass it to parallel.
+    // To do this, you need to wrap the code below in { }, and either return
+    // that explicitly, or use { -> } syntax.
+    return {
+       // Job parameters can be added to this step
+       build jobFullName
+    }
+}
+
+// Return a map of jobs of the Downstreams that needed to be called
+def getParallelStepsForDownstreamJobs(listOfDownstreamJobs) {
+
+    String regexString = '(' + listOfDownstreamJobs.replaceAll(',', '|') + ')'
+
+    j = jobs(regexString)
+    for (int i=0; i < j.size(); i++) {
+        stepsForParallel["${j[i].name}"] = transformIntoStep(j[i].fullName)
+    }
+
+    return stepsForParallel
+}
+
